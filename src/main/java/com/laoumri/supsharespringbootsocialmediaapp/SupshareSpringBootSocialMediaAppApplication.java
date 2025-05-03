844492a68a542f4b4511692b6fa9cdb6788dd7aa
ISSAM LAOUMRI <issamlaoumri@gmail.com>
2025-05-03 15:05:04 +0100

    initial commit

---
+++ src/main/java/com/laoumri/supsharespringbootsocialmediaapp/SupshareSpringBootSocialMediaAppApplication.java
@@ -0,0 +1,13 @@
+package com.laoumri.supsharespringbootsocialmediaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupshareSpringBootSocialMediaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupshareSpringBootSocialMediaAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/laoumri/supsharespringbootsocialmediaapp/SupshareSpringBootSocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laoumri.supsharespringbootsocialmediaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupshareSpringBootSocialMediaAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
